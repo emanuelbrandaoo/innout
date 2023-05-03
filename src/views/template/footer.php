7ec4be5b59873a222263752111c8fd037890c576
Emanuel Brandão <emanuelfelipebrandao@gmail.com>
2023-05-02 21:44:25 -0300

    login

---
+++ src/views/template/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por Emanuel Brandão</span>
+</footer>
+<script src="/assets/js/app.js"></script>
+</body>
+
+</html>
