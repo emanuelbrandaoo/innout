601f1b788260bc15747b56a57637cca82e67a366
Emanuel Brandão <emanuelfelipebrandao@gmail.com>
2023-05-10 18:49:06 -0300

    final

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,71 +1,64 @@
 <?php
-session_start();
-requireValidSession();
+Database::executeSQL('DELETE FROM working_hours');
+Database::executeSQL('DELETE FROM users WHERE id > 5');
 
-if($_SESSION['user']->is_admin == 1) {
-    Database::executeSQL('TRUNCATE TABLE working_hours');
-    Database::executeSQL('DELETE FROM users WHERE id > 5');
+function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate)
+{
+    $regularDayTemplate = [
+        'time1' => '08:00:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '17:00:00',
+        'worked_time' => DAILY_TIME
+    ];
 
-    function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
-        $regularDayTemplate = [
-            'time1' => '08:00:00',
-            'time2' => '12:00:00',
-            'time3' => '13:00:00',
-            'time4' => '17:00:00',
-            'worked_time' => DAILY_TIME
-        ];
-        
-        $extraHourDayTemplate = [
-            'time1' => '08:00:00',
-            'time2' => '12:00:00',
-            'time3' => '13:00:00',
-            'time4' => '18:00:00',
-            'worked_time' => DAILY_TIME + 3600
-        ];
-        
-        $lazyDayTemplate = [
-            'time1' => '08:30:00',
-            'time2' => '12:00:00',
-            'time3' => '13:00:00',
-            'time4' => '17:00:00',
-            'worked_time' => DAILY_TIME - 1800
-        ];
+    $extraHourDayTemplate = [
+        'time1' => '08:00:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '18:00:00',
+        'worked_time' => DAILY_TIME + 3600
+    ];
 
-        $value = rand(0, 100);
-        if ($value <= $regularRate) {
-            return $regularDayTemplate;
-        } elseif ($value <= $regularRate + $extraRate) {
-            return $extraHourDayTemplate;
-        } else {
-            return $lazyDayTemplate;
-        }
+    $lazyDayTemplate = [
+        'time1' => '08:30:00',
+        'time2' => '12:00:00',
+        'time3' => '13:00:00',
+        'time4' => '17:00:00',
+        'worked_time' => DAILY_TIME - 1800
+    ];
+
+    $value = rand(0, 100);
+    if ($value <= $regularRate) {
+        return $regularDayTemplate;
+    } elseif ($value <= $regularRate + $extraRate) {
+        return $extraHourDayTemplate;
+    } else {
+        return $lazyDayTemplate;
     }
-
-    function populateWorkingHours($userId, $initialData, $regularRate, $extraRate, $lazyRate) {
-        $currentDate = $initialData;
-        $yesterday = new DateTime();
-        $yesterday->modify('-1 day');
-        $columns = ['user_id' => $userId, 'work_date' => $currentDate];
-
-        while(isBefore($currentDate, $yesterday)) {
-            if(!isWeekend($currentDate)) {
-                $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
-                $columns = array_merge($columns, $template);
-                $workingHours = new WorkingHours($columns);
-                $workingHours->insert();
-            }
-            $currentDate = getNextDay($currentDate)->format('Y-m-d');
-            $columns['work_date'] = $currentDate;
-        }
-    }
-
-    $lastMonth = strtotime('first day of january');
-
-    populateWorkingHours(1, date('Y-m-d', $lastMonth), 70, 20, 10); // Admin
-    populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5); // Seu Barriga
-    populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70); // Seu Madruga
-
-    addSuccessMsg('Dados gerados com sucesso.');
 }
 
-header('Location: day_records.php');
+function populateWorkingHours($userId, $initialData, $regularRate, $extraRate, $lazyRate)
+{
+    $currentDate = $initialData;
+    $yesterday = new DateTime();
+    $yesterday->modify('-1 day');
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+
+    while (isBefore($currentDate, $yesterday)) {
+        if (!isWeekend($currentDate)) {
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->insert();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
+}
+
+$firstMonth = strtotime('first day of january');
+
+populateWorkingHours(1, date('Y-m-d', $firstMonth), 70, 20, 10); // Admin
+populateWorkingHours(3, date('Y-m-d', $firstMonth), 20, 75, 5); // Seu Barriga
+populateWorkingHours(4, date('Y-m-d', $firstMonth), 20, 10, 70); // Seu Madruga
--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -3,6 +3,6 @@ session_start();
 requireValidSession();
 
 $date = (new Datetime())->getTimestamp();
-$today = ucfirst(strftime('%B, %d de %Y', $date));
+$today = (strftime('%d de %B de %Y', $date));
 
 loadTemplateView('day_records', ['today' => $today]);
